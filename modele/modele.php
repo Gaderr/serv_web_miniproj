d4fa8208d7da421ebd505462082e0c36c143e274
Gabriel Derrien <gabriel.derrien@etu.univ-nantes.fr>
2017-11-20 23:48:10 +0100

    Ecriture des méthodes de déplacement des billes sur le plateau

--- modele/modele.php
+++ modele/modele.php
@@ -2,145 +2,176 @@
 
 class Modele
 {
-  private $connexion;
-  private $plateau;
+    private $connexion;
+    private $plateau;
 
-  // Constructeur
-  public function __construct()
-  {
-    $plateau = array(); //La matrice/plateau de jeu
-
-    //Initialisation du plateau
-    //Chaque case est définie par un caractère :
-    //  - 'x' = Non jouable
-    //  - 'o' = Bille présente & case jouable
-    //  - 'u' = Bille absente & case jouable
-    //On commence par définir toutes les cases non jouables
-    for ($ligne=0; $ligne<$nblignes; $ligne++)
+    // Constructeur
+    public function __construct()
     {
-      for ($colonne=0; $colonne<$nbcolonnes; $colonne++)
-      {
-        $plateau[$ligne][$colonne] = 'x'; //une case non jouable
-      }
+        $plateau = array(); //La matrice/plateau de jeu
+
+        //Initialisation du plateau
+        //Chaque case est définie par un caractère :
+        //  - 'x' = Non jouable
+        //  - 'o' = Bille présente & case jouable
+        //  - 'u' = Bille absente & case jouable
+        //On commence par définir toutes les cases non jouables
+        for ($ligne=0; $ligne<$nblignes; $ligne++)
+        {
+            for ($colonne=0; $colonne<$nbcolonnes; $colonne++)
+            {
+                $plateau[$ligne][$colonne] = 'x'; //une case non jouable
+            }
+        }
+
+        //Définition des emplacements
+        //On définit les cases jouables par dessus les non-jouables
+        for($l = 2; $l < 4; $l++)//Les lignes 3, 4, 5 sont jouables
+        {
+            for($c = 0; $c < 6; $c++)
+            {
+                $plateau[$l][$c] = 'o'; //o = un emplacement
+            }
+        }
+        for($c = 2; $c < 4; $c++)//Les colonnes 3, 4, 5 sont jouables
+        {
+            for($l = 0; $l < 6; $c++)
+            {
+                $plateau[$l][$c] = 'o';
+            }
+        }
+
+        //résultat :
+        //  +---------------> axe X
+        //  | X X o o o X X
+        //  | X X o o o X X
+        //  | o o o o o o o
+        //  | o o o o o o o
+        //  | o o o o o o o
+        //  | X X o o o X X
+        //  | X X o o o X X
+        //  v
+        //  axe Y
+
+        try
+        {
+            $chaine="mysql:host=".HOST.";dbname=".BD;
+            $this->connexion = new PDO($chaine,LOGIN,PASSWORD);
+            $this->connexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e)
+        {
+            $exception=new ConnexionException("problème de connexion à la base");
+            throw $exception;
+        }
     }
 
-    //Définition des emplacements
-    //On définit les cases jouables par dessus les non-jouables
-    for($l = 2; $l < 4; $l++)//Les lignes 3, 4, 5 sont jouables
+    public function startGame(x, y) //Sélection de la première bille à supprimer pour commencer à jouer
     {
-      for($c = 0; $c < 6; $c++)
-      {
-        $plateau[$l][$c] = 'o'; //o = un emplacement
-      }
+        if($this->plateau[x][y] != 'x' && $this->plateau[x][y] == 'o')
+        {
+            $this->plateau[x][y] = 'u';
+        }
     }
-    for($c = 2; $c < 4; $c++)//Les colonnes 3, 4, 5 sont jouables
+
+    public function reInit()
     {
-      for($l = 0; $l < 6; $c++)
-      {
-        $plateau[$l][$c] = 'o';
-      }
+        for($l = 2; $l < 4; $l++)
+        {
+            for($c = 0; $c < 6; $c++)
+            {
+                $plateau[$l][$c] = 'o';
+            }
+        }
+        for($c = 2; $c < 4; $c++)
+        {
+            for($l = 0; $l < 6; $c++)
+            {
+                $plateau[$l][$c] = 'o';
+            }
+        }
     }
-    //résultat :
-    //  +---------------> axe X
-    //  | X X o o o X X
-    //  | X X o o o X X
-    //  | o o o o o o o
-    //  | o o o o o o o
-    //  | o o o o o o o
-    //  | X X o o o X X
-    //  | X X o o o X X
-    //  v
-    //  axe Y
 
-    try
+    public function moveUp(x, y)
     {
-      $chaine="mysql:host=".HOST.";dbname=".BD;
-      $this->connexion = new PDO($chaine,LOGIN,PASSWORD);
-      $this->connexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        if($this->plateau[x][y - 1] == 'o' && $this->plateau[x][y - 2] == 'u')
+        {
+            $this->plateau[x][y - 1] == 'u'; //bille supprimée
+            $this->plateau[x][y - 2] == 'o'; //bille placée
+            $this->plateau[x][y] == 'u'; //bille supprimée
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
-    catch(PDOException $e)
+
+    public function moveDown(x, y)
     {
-      $exception=new ConnexionException("problème de connexion à la base");
-      throw $exception;
+        if($this->plateau[x][y + 1] == 'o' && $this->plateau[x][y - 2] == 'u')
+        {
+            $this->plateau[x][y + 1] == 'u'; //bille supprimée
+            $this->plateau[x][y + 2] == 'o'; //bille placée
+            $this->plateau[x][y] == 'u'; //bille supprimée
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
-  }
 
-  public function startGame(x, y) //Sélection de la première bille à supprimer pour commencer à jouer
-  {
-    if($this->plateau[x][y] != 'x' && $this->plateau[x][y] == 'o')
+    public function moveLeft(x, y)
     {
-      $this->plateau[x][y] = 'u';
+        if($this->plateau[x - 1][y] == 'o' && $this->plateau[x][y - 2] == 'u')
+        {
+            $this->plateau[x - 1][y] == 'u'; //bille supprimée
+            $this->plateau[x - 2][y] == 'o'; //bille placée
+            $this->plateau[x][y] == 'u'; //bille supprimée
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
-  }
 
-  public function reInit()
-  {
-    for($l = 2; $l < 4; $l++)
+    public function moveRight(x, y)
     {
-      for($c = 0; $c < 6; $c++)
-      {
-        $plateau[$l][$c] = 'o';
-      }
+        if($this->plateau[x + 1][y] == 'o' && $this->plateau[x][y - 2] == 'u')
+        {
+            $this->plateau[x + 1][y] == 'u'; //bille supprimée
+            $this->plateau[x + 2][y] == 'o'; //bille placée
+            $this->plateau[x][y] == 'u'; //bille supprimée
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
-    for($c = 2; $c < 4; $c++)
+
+    //Déconnexion de la base
+    public function deconnexion()
     {
-      for($l = 0; $l < 6; $c++)
-      {
-        $plateau[$l][$c] = 'o';
-      }
+        $this->connexion=null;
     }
-  }
 
-  public function moveUp(x, y)
-  {
-    if($this->plateau[x][y - 1] == 'o' && $this->plateau[x][y - 2] == 'u')
+    public function checkAuth($pseudo, $pass)
     {
-      $this->plateau[x][y - 1] == 'u'; //bille supprimée
-      $this->plateau[x][y - 2] == 'o'; //bille placée
-      $this->plateau[x][y] == 'u'; //bille supprimée
-      return true;
+        $statement = $this->connexion->prepare("SELECT motDePasse FROM joueurs where pseudo=?;");
+        $statement->bindParam(1, $pseudo);
+        $statement->execute();
+        $result=$statement->fetch(PDO::FETCH_ASSOC);
+        if(crypt($pass, $result['motDePasse']) == $result['motDePasse'])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
-    else
-    {
-      return false;
-    }
-  }
-
-  public function moveDown(x, y)
-  {
-
-  }
-
-  public function moveLeft(x, y)
-  {
-
-  }
-
-  public function moveRight(x, y)
-  {
-
-  }
-
-  //Déconnexion de la base
-  public function deconnexion()
-  {
-     $this->connexion=null;
-  }
-
-  public function checkAuth($pseudo, $pass)
-  {
-    $statement = $this->connexion->prepare("SELECT motDePasse FROM joueurs where pseudo=?;");
-    $statement->bindParam(1, $pseudo);
-    $statement->execute();
-    $result=$statement->fetch(PDO::FETCH_ASSOC);
-    if(crypt($pass, $result['motDePasse']) == $result['motDePasse'])
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-  }
-
- ?>
+}
+?>
